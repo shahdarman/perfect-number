241cf9338849a9ba983681b96624b18a2859ab82
shahdarman <121302502+shahdarman@users.noreply.github.com>
2022-12-23 17:37:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello sweety");
+
+            int number, sum = 0, n;
+
+            Console.Write("enter the Number");
+            
+            number = int.Parse(Console.ReadLine());
+           
+            n = number;
+            
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+               
+                {
+                    sum = sum + i;
+                }
+            }
+           
+            if (sum == n)
+            
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+               
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+               
+                Console.ReadLine();
+
+                Console.WriteLine("Thank You , Have A Nice Day");
+            }
+        }
+    }
+}
